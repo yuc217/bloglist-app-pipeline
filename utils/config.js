43e61f403029b22b9272393ce3bcec26e8ad2a03
yuc217 <14282858+yuc217@users.noreply.github.com>
2025-01-09 18:08:48 +0200

    fix style

--- utils/config.js
+++ utils/config.js
@@ -5,10 +5,10 @@ const PORT = process.env.PORT
 const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
 
 if (!process.env.MONGODB_URI) {
-  throw new Error('Missing MONGODB_URI environment variable');
+  throw new Error('Missing MONGODB_URI environment variable')
 }
 if (!process.env.TEST_MONGODB_URI) {
-  throw new Error('Missing TEST_MONGODB_URI environment variable');
+  throw new Error('Missing TEST_MONGODB_URI environment variable')
 }
 
 module.exports = {
